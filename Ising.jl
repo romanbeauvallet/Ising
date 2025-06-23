316c98713de67f734c33438e140f383d73e96b8e
Olivier Gauthé <ogauthe@flatironinstitute.org>
2025-06-23 13:22:05 -0400

    determnistic TEBD

--- Ising.jl
+++ Ising.jl
@@ -20,11 +20,11 @@ end
 
 J = 1.0
 h = 0.0
-n = 10
+niter = 1
 N = 20 #ATTENTION N doit être pair
 d = 2
-D0 = 20
-Dmax = 1000
+D0 = 4
+Dmax = 100
 cutoff = 1e-12
 rejected_weight = 1e-20
 
@@ -50,6 +50,7 @@ function isingTensor(Beta, J, h)
     return T
 end
 
+#=
 """
 N - number of sites
 d - dimension of physical indices
@@ -88,46 +89,49 @@ function ising2Dunit(MPS, J::Float64, h::Float64, Beta::Float64, n, cutoff, Dmax
         mpsnew[i], mpsnew[i+1] = tebd(mpsleft[i], mpsright[i+1], G, Dmax, cutoff, rejected_weight, side)
     end
 end
+=#
 
 """
 one sweep of TEBD for the 2D Ising model with a different method
 always start with a right canonical mps, sweep : left->right->left
 
 """
-function tebd_sweep(mps, Beta, J, h, Dmax, rejected_weight, cutoff)
-    p = length(mps)
-    gate = isingTensor(Beta, J, h) # Example parameters for the Ising tensor
+function tebd_sweep(mps, gate, Dmax, rejected_weight, cutoff)
+    #@show "BEGIN LEFT TO RIGHT SWEEP"
+    N = length(mps)
     #%% FIRST PART ON EVEN COUPLE
-    mpsnew = Vector{eltype(mps)}(undef, p)
-    tempmps = Vector{eltype(mps)}(undef, p)
+    mpsnew = Vector{eltype(mps)}(undef, N)
     right = canonicalright(mps)[1]
     #println.(size.(right))
-    for i in 1:2:p-3 #n est even
+    for i in 1:2:N-3 #n est even
+        #@show i, i + 1
         #@show i, size(right[i])
-        mpsnew[i], tempmps[i+1] = tebd2(right[i], right[i+1], gate, "right", Dmax, cutoff, rejected_weight)
+        mpsnew[i], temp = tebd_step(right[i], right[i+1], gate, "right", Dmax, cutoff, rejected_weight)
         #@show size(mpsnew[i])
         #changement de jauge
-        U, S, V = svd(reshape(tempmps[i+1], size(tempmps[i+1], 1) * size(tempmps[i+1], 2), size(tempmps[i+1], 3)))
-        mpsnew[i+1] = reshape(U, size(tempmps[i+1], 1), size(tempmps[i+1], 2), size(U, 2))
-        right[i+2] = tensorcontract(Diagonal(S) * V', (-1, 1), right[i+2], (-2, 1, -3))
-        right[i+2] = permutedims(right[i+2], (2, 1, 3)) # Permute dimensions to match the original order
+        U, S, V = svd(reshape(temp, size(temp, 1) * size(temp, 2), size(temp, 3)))
+        mpsnew[i+1] = reshape(U, size(temp, 1), size(temp, 2), size(U, 2))
+        right[i+2] = tensorcontract((-2, -1, -3), Diagonal(S) * V', (-1, 1), right[i+2], (-2, 1, -3))
     end
+    #@show "special case", N - 1, N
     #il reste les deux derniers à faire à la main ou alors on rallonge le mps de 1 tenseur au départ qui rammasse les poids de la svd puis on cut
-    mpsnew[p-1], mpsnew[p] = tebd2(right[p-1], right[p], gate, "right", Dmax, cutoff, rejected_weight)
+    mpsnew[N-1], last_site = tebd_step(right[N-1], right[N], gate, "right", Dmax, cutoff, rejected_weight)
     #mettre le dernier dans la jauge gauche
-    Z = permutedims(mpsnew[p], (2, 3, 1))
-    A, B, C = svd(reshape(Z, size(mpsnew[p], 2), size(mpsnew[p], 1) * size(mpsnew[p], 3)))
-    mpsnew[p] = permutedims(reshape(C', size(C', 1), size(Z, 2), size(Z, 3)), (3, 1, 2))
-    mpsnew[p-1] = tensorcontract(mpsnew[p-1], (-1, -2, 1), A * Diagonal(B), (1, -3))
-    for i in p-1:-2:3
+    last_site = permutedims(last_site, (2, 3, 1))  # left-right-phys
+    A, B, C = svd(reshape(last_site, size(last_site, 1), size(last_site, 2) * size(last_site, 3)))
+    mpsnew[N] = permutedims(reshape(C', size(C', 1), size(last_site, 2), size(last_site, 3)), (3, 1, 2))
+    mpsnew[N-1] = tensorcontract((-1, -2, -3), mpsnew[N-1], (-1, -2, 1), A * Diagonal(B), (1, -3))
+    #@show "BEGIN RIGHT TO LEFT SWEEP"
+    for i in N-1:-2:3
+        #@show i - 1, i
         #@show i
         #@show size(right[i-1]), size(right[i])
-        tempmps[i-1], mpsnew[i] = tebd2(mpsnew[i-1], mpsnew[i], gate, "left", Dmax, cutoff, rejected_weight)
+        temp, mpsnew[i] = tebd_step(mpsnew[i-1], mpsnew[i], gate, "left", Dmax, cutoff, rejected_weight)
         #changement de jauge
-        tempmps[i-1] = permutedims(tempmps[i-1], (2, 1, 3)) # Permute dimensions to reshape correctly (gauche, physique, droite)
-        U, S, V = svd(reshape(tempmps[i-1], size(tempmps[i-1], 1), size(tempmps[i-1], 2) * size(tempmps[i-1], 3)))
-        mpsnew[i-1] = permutedims(reshape(V', size(V', 1), size(tempmps[i-1], 2), size(tempmps[i-1], 3)), (2, 1, 3))
-        mpsnew[i-2] = tensorcontract(mpsnew[i-2], (-1, -2, 1), U * Diagonal(S), (1, -3))
+        temp = permutedims(temp, (2, 1, 3)) # Permute dimensions to reshape correctly (gauche, physique, droite)
+        U, S, V = svd(reshape(temp, size(temp, 1), size(temp, 2) * size(temp, 3)))
+        mpsnew[i-1] = permutedims(reshape(V', size(V', 1), size(temp, 2), size(temp, 3)), (2, 1, 3))
+        mpsnew[i-2] = tensorcontract((-1, -2, -3), mpsnew[i-2], (-1, -2, 1), U * Diagonal(S), (1, -3))
     end
     return mpsnew
 end
@@ -146,20 +150,13 @@ side - side of the lattice
 
 returns the final MPS for the 2D Ising model after applying TEBD and n sweeps
 """
-function ising2D(N::Int, d, D, J::Float64, h::Float64, Beta::Float64, n, cutoff, Dmax, side, rejected_weight)
-    # Create the MPS for the 2D Ising model
-    mps = MPS(N, d, D)
-    # Sweeps
-    for i in 1:n
-        mps = ising2Dunit(mps, J, h, Beta, n, cutoff, Dmax, side, rejected_weight)
-    end
-    return mps
-end
-
-function ising2D2(N, D0, d, J, h, Beta, n, Dmax, rejected_weight, cutoff)
+function ising2D(N, D0, d, J, h, β, n_sweeps, Dmax, rejected_weight, cutoff)
+    #@show "Begin"
+    gate = isingTensor(β, J, h) # Example parameters for the Ising tensor
     mps = init_random_mps(N, d, D0)
-    for i in 1:n
-        mps = tebd_sweep(mps, Beta, J, h, Dmax, rejected_weight, cutoff)
+    for i in 1:n_sweeps
+        #@show "SWEEP i =", i
+        mps = tebd_sweep(mps, gate, Dmax, rejected_weight, cutoff)
     end
     return mps
 end
@@ -171,52 +168,55 @@ i - site du MPS sur lequel on calcule l'énergie
 
 return l'énergie sur le site i du MPS
 """
-function energyIsing(mps, J, i)#mettre un ! au debut du nom de la fonction pour spécifier que la fonction modifie l'input
+function energyIsing(mps, J, i_meas)#mettre un ! au debut du nom de la fonction pour spécifier que la fonction modifie l'input
     #mettre en left et right canonical par rapport au tenseur i
     #garder seulement le centre car toute l'information du mps y est
     #contracter entre le tenseur et le dagger la matrice énergie et contracter tous les axes
     #vérifier que la norme du mps est bien 1 (si pas le cas normaliser la contraction précédente par la norme du tenseur)
 
     #%% orthonormalisation
-    m = length(mps)
-    if !(1 < i < m)
-        error("site is not in the mps")
+    N = length(mps)
+    if !(1 < i_meas < N)
+        throw(ArgumentError("site is not in the mps"))
     end
-    Z = energytensor(J)
+    bond_operator = energytensor(J)
     #@show size(MPS[1:i]), size(canonicalleft(MPS[1:i])[1])
-    mps[begin:i] = canonicalleft(mps[begin:i])[1]
+    center_canonical_mps = deepcopy(mps)
+    center_canonical_mps[begin:i_meas], _ = canonicalleft(center_canonical_mps[begin:i_meas])
     #verif
-    I = tensorcontract(mps[i-1], (1, 2, -1), conj(mps[i-1]), (1, 2, -2))
-    @show I ≈ LinearAlgebra.I(size(I, 1))
-    I2 = tensorcontract(mps[i+1], (1, -1, 2), conj(mps[i+1]), (1, -2, 2))
-    @show I2 ≈ LinearAlgebra.I(size(I2, 1))
+    I = tensorcontract(center_canonical_mps[i_meas-1], (1, 2, -1), conj(center_canonical_mps[i_meas-1]), (1, 2, -2))
+    @assert I ≈ LinearAlgebra.I(size(I, 1))
+    I2 = tensorcontract(center_canonical_mps[i_meas+1], (1, -1, 2), conj(center_canonical_mps[i_meas+1]), (1, -2, 2))
+    @assert I2 ≈ LinearAlgebra.I(size(I2, 1))
     #%% contraction
-    @show size(mps[i]), typeof(mps[i])
-    D = permutedims(conj(mps[i]), (1, 3, 2))
-    N = tensorcontract(mps[i], (1, 2, 3), D, (1, 3, 2))
-    @show N
-    inter = tensorcontract(mps[i], (1, -1, -2), Z, (1, -3))
-    E = tensorcontract(inter, (1, 2, 3), D, (3, 2, 1))
-    return E[] #mps
+    ket = center_canonical_mps[i_meas]
+    #@show size(ket), typeof(ket)
+    #mps_norm = tensorcontract(ket, (1, 2, 3), conj(ket), (1, 2, 3))
+    #@show mps_norm
+    inter = tensorcontract((-1, -2, -3), conj(ket), (1, -2, -3), bond_operator, (1, -1))
+    E = tensorcontract(inter, (1, 2, 3), ket, (1, 2, 3))[]
+    return E
 end
 
 #on fait les données et on trace
 gr()
 
 site_measure = N ÷ 2
-Betalist = collect(0.1:0.1:10)
-MPSlist = map(β -> ising2D2(N, D0, d, J, h, β, n, Dmax, rejected_weight, cutoff), Betalist)
-Elist = map(mps -> energyIsing(mps, J, site_measure), MPSlist)
+#Betalist = collect(0.1:0.1:10)
+Betalist = [0.1, 0.2]
 Eexact = ExactEnergy.(Betalist)
 
-#display(current())
-mps = init_random_mps(N, d, D0)
-e1 = energyIsing(deepcopy(mps), J, site_measure)
-e2 = energyIsing(deepcopy(mps), J, site_measure)
+MPSlist = map(β -> ising2D(N, D0, d, J, h, β, 100, Dmax, rejected_weight, cutoff), Betalist);
+Elist = map(mps -> energyIsing(mps, J, site_measure), MPSlist)
 
-@show e1, e2
-
-plot(Betalist, Elist, label="TEBD", xlabel="\$\\beta\$", ylabel="Energy")
-plot!(Betalist, Eexact, label="exact")
+#plot(Betalist, Elist, label="TEBD", xlabel="\$\\beta\$", ylabel="Energy")
+#plot!(Betalist, Eexact, label="exact")
 
 #en julia on met les lignes de plot soit à la toute fin du code soit on ajoute un display(current())
+println.(Betalist .=> Elist)
+
+N = 20
+β = 0.1
+niter = 100
+mps = ising2D(N, D0, d, J, h, β, niter, Dmax, rejected_weight, cutoff);
+println(nothing)
